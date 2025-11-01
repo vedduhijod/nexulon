adcd046827981889a869556a3b3e8bdc91911688
Ved Duhijod <vedduhijod@gmail.com>
2025-11-02 03:31:13 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+
+export default function Home() {
+  const { setTheme } = useTheme();
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Click Me</Button>
+      <Button onClick={() => setTheme('dark')}>Dark</Button>
+      <Button onClick={() => setTheme('light')}>Light</Button>
+    </div>
+  );
+}
