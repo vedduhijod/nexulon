f526d04230d9db6904e19b3fde5bb39e1daffe6b
Ved Duhijod <vedduhijod@gmail.com>
2025-11-05 22:24:31 +0530

    "added faults "

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -18,4 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = app.firestore(app, "default");
+export const db = app.firestore(app);
